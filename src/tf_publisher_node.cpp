b6b4264da8f04c10989c3f85b20efe252d0aa3e8
caliandro-arturo <arturo.caliandro@mail.polimi.it>
2023-06-18 19:10:24 +0200

    implement tf publisher node

--- src/tf_publisher_node.cpp
+++ src/tf_publisher_node.cpp
@@ -2,8 +2,44 @@
  * TF publisher
  */
 #include <ros/ros.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
+#define SUB_TOPIC "/t265/odom"
 
-int main(int argc, char* argv[])
+
+// Handle the subscription and the TF publishing.
+class OdometricPubSub
+{
+public:
+    OdometricPubSub()
+    {
+        sub = n.subscribe(SUB_TOPIC, 1000, &OdometricPubSub::callback, this);
+    }
+
+    // Send the odometry information to the tf transform broadcaster.
+    void callback(const nav_msgs::Odometry::ConstPtr &msg)
+    {
+        tf::Transform transform;
+        tf::Vector3 v;
+        tf::pointMsgToTF(msg->pose.pose.position, v);
+        transform.setOrigin(v);
+        tf::Quaternion q;
+        tf::quaternionMsgToTF(msg->pose.pose.orientation, q);
+        transform.setRotation(q);
+        br.sendTransform(tf::StampedTransform(transform, msg->header.stamp, "odom", "t265"));
+    }
+
+private:
+    ros::NodeHandle n;
+    ros::Subscriber sub;
+    tf::TransformBroadcaster br;
+};
+
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "tf_publisher");
+    ROS_INFO("Node started.");
+    OdometricPubSub odom;
+    ros::spin();
+    return 0;
 }
